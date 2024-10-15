26a7f866ff4ea0d1c490fd18f08b3d98ad3ea6cb
DT0072 <dtkelvin9@gmail.com>
2024-10-15 17:56:13 +0800

    Environment Setup [Topbar, Footer, Login, Homepage, Cart, Checkout, and Contact Us]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CheckoutItemComponent } from './checkout-item/checkout-item.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    TopbarComponent,
+    FooterComponent,
+    CartComponent,
+    UserLoginComponent,
+    CartItemComponent,
+    CheckoutComponent,
+    CheckoutItemComponent,
+    ContactusComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/checkout-item/checkout-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-item',
+  templateUrl: './checkout-item.component.html',
+  styleUrl: './checkout-item.component.css'
+})
+export class CheckoutItemComponent {
+
+}
